45f992a5deaf280faeb7a4f2f978e32f3a10df91
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-01-28 17:52:04 +0300

    update

--- app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
@@ -1,43 +1,84 @@
 package com.example.statemanagementcompose
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.statemanagementcompose.ui.theme.StateManagementComposeTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StateManagementComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        color = Color.LightGray
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Hello!",
+                fontSize = 26.sp
+            )
+
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Button(onClick = {
+                println("clicked!")
+            }) {
+                Text(text = "Button")
+                Text(text = "Test")
+            }
+
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.bmw1),
+                contentDescription = null)
+
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(
+                imageVector = ImageVector.vectorResource(
+                    id = R.drawable.ic_launcher_foreground),
+                contentDescription = null)
+
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(painter = ColorPainter(Color.Blue),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp, 20.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    StateManagementComposeTheme {
-        Greeting("Android")
-    }
+    MainScreen()
 }
