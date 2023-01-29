271faa0037d6fafba4d4a4c202b6e609c90ca87b
Çağatay İnyurt <cagatayinyurt@outlook.com>
2023-01-29 17:08:20 +0300

    update

--- app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/example/statemanagementcompose/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.statemanagementcompose
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,48 +29,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    Surface(
-        color = Color.LightGray
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+    // State should always stay on top
+    var myString = remember { mutableStateOf("") }
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Hello!",
-                fontSize = 26.sp
-            )
-
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Button(onClick = {
-                println("clicked!")
-            }) {
-                Text(text = "Button")
-                Text(text = "Test")
+            ) {
+            SpecialText(string = "Test")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialText(string = "Android")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialTextField(myString.value) {
+                myString.value = it
             }
-
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(
-                bitmap = ImageBitmap.imageResource(id = R.drawable.bmw1),
-                contentDescription = null)
-
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(
-                imageVector = ImageVector.vectorResource(
-                    id = R.drawable.ic_launcher_foreground),
-                contentDescription = null)
-
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(painter = ColorPainter(Color.Blue),
-                contentDescription = null,
-                modifier = Modifier.size(20.dp, 20.dp))
         }
     }
+}
+
+@Composable
+fun SpecialText(string: String) {
+    Text(text = string,
+        fontSize = 20.sp,
+        fontStyle = FontStyle.Italic,
+        fontFamily = FontFamily.SansSerif)
+}
+
+@Composable
+fun SpecialTextField(string: String, function: (String) -> Unit) {
+    TextField(value = string, onValueChange = function, modifier = Modifier.padding(5.dp))
 }
 
 @Preview(showBackground = true)
